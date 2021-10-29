2cd0d243bc3b3bad7bba2a3ea03b3c04e33d5003
NiKuma0 <ybr811@gmail.com>
2021-10-29 13:18:06 +0300

    added cancel button

--- keyboard.py
+++ keyboard.py
@@ -14,6 +14,7 @@ form_buttons = (
     InlineKeyboardButton(text='-1 час', callback_data='hours -1'),
     InlineKeyboardButton(text='1 день', callback_data='days 1'),
     InlineKeyboardButton(text='-1 день', callback_data='days -1'),
+    InlineKeyboardButton(text='Отмена', callback_data='cancel'),
     InlineKeyboardButton(text='Готово!', callback_data='done'),
 
 )
@@ -21,8 +22,8 @@ form_keyboard = InlineKeyboardMarkup(row_width=2)
 form_keyboard.add(*form_buttons)
 
 main_buttons = (
-    KeyboardButton(text='Что ты можешь?'),
+    # KeyboardButton(text='Что ты можешь?'),
     KeyboardButton(text='Новая задача!'),
 )
-main_keyboard = ReplyKeyboardMarkup(row_width=2, input_field_placeholder='\/ \/ \/')
+main_keyboard = ReplyKeyboardMarkup(row_width=1, input_field_placeholder='\/ \/ \/')
 main_keyboard.add(*main_buttons)
